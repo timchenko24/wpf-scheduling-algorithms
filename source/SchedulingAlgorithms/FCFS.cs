6dea9a5fede5aed1477cdf4e5dacfc29823f08a1
Tim Chenskiy <timadraft@gmail.com>
2020-07-30 19:44:40 +0300

    FCFS class

---
+++ source/SchedulingAlgorithms/FCFS.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchedulingAlgorithms
+{
+    public class FCFS : SchedulingAlgorithm
+    {
+        public FCFS(JobQueue workQueue, IComparer<Job> comparer) : base(workQueue, comparer) { }
+
+        public override Job NextStep(int simulationTime)
+        {
+            UpdateReadyQueue(simulationTime);
+            if (!isBusy)
+            {
+                if (readyQueue.IsEmpty())
+                {
+                    return null;
+                }
+                isBusy = true;
+                SetCurrentJob();
+            }
+            return WorkInCPU(simulationTime);
+        }
+
+        protected override Job WorkInCPU(int simulationTime)
+        {
+            currentJob.JobWorked(simulationTime);
+            if (currentJob.RemainingTime == 0)
+            {
+                isBusy = false;
+            }
+            return currentJob;
+        }
+    }
+}
