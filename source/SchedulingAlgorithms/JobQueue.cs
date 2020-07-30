48ce45f9207e0000d4d1823031e3de4d19500c61
Tim Chenskiy <timadraft@gmail.com>
2020-07-30 18:44:41 +0300

    JobQueue class added

---
+++ source/SchedulingAlgorithms/JobQueue.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchedulingAlgorithms
+{
+    public class JobQueue
+    {
+        public List<Job> JobList { get; set; }
+
+        public JobQueue()
+        {
+            JobList = new List<Job>();
+        }
+
+        public JobQueue(List<Job> list)
+        {
+            JobList = new List<Job>();
+            JobList.AddRange(list);
+        }
+
+        public Job GetJobById(int id)
+        {
+            return JobList.ElementAt(id);
+        }
+
+        public void Remove(int id)
+        {
+            JobList.RemoveAt(id);
+        }
+
+        public void AddJob(Job job)
+        {
+            JobList.Add(job);
+        }
+
+        public bool IsEmpty()
+        {
+            return (JobList.Count == 0);
+        }
+
+        public int GetSize()
+        {
+            return JobList.Count;
+        }
+    }
+}
