fc7f3827e87ad7f5a559316324c471ccf34a8a8c
Tim Chenskiy <timadraft@gmail.com>
2020-08-01 18:45:32 +0300

    addProcess click

--- source/SchedulingAlgorithms/MainWindow.xaml.cs
+++ source/SchedulingAlgorithms/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,25 @@ namespace SchedulingAlgorithms
     /// </summary>
     public partial class MainWindow : Window
     {
+        InputController input;
+        JobQueue jobQueue;
+
         public MainWindow()
         {
             InitializeComponent();
+            input = new InputController();
+            jobQueue = new JobQueue();
+            DataContext = input;
+            dgProcessesTable.ItemsSource = jobQueue.JobList;
+            //List<Job> t = new List<Job>() { new Job(1, 1, 3, 1), new Job(0, 1, 1, 1), new Job(2, 1, 5, 2), new Job(3, 1, 5, 0) };
+            //JobByBurstComparer c = new JobByBurstComparer();
+            //t.Sort(c);
+        }
+
+        private void btnAddProcess_Click(object sender, RoutedEventArgs e)
+        {
+            jobQueue.AddJob(new Job(input.JobNumber, input.ArrivalTime, input.Burst, input.Priority));
+            dgProcessesTable.Items.Refresh();
         }
     }
 }
