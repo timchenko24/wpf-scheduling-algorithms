6e5024726d4f686ee1dded4490f8800bb4a6c9f5
Tim Chenskiy <timadraft@gmail.com>
2020-08-06 17:39:15 +0300

    btnReset

--- source/SchedulingAlgorithms/MainWindow.xaml.cs
+++ source/SchedulingAlgorithms/MainWindow.xaml.cs
@@ -30,18 +30,16 @@ namespace SchedulingAlgorithms
         {
             InitializeComponent();
             tbProcessName.IsEnabled = false;
+            btnStartSimulation.IsEnabled = false;
             input = new InputController
             {
                 JobNumber = 0
             };
             jobQueue = new JobQueue();
             simulation = new Simulation();
-            chart = new BarChart(Chart);
+            chart = new BarChart(Chart, simulation);
             DataContext = input;
             dgProcessesTable.ItemsSource = jobQueue.JobList;
-            //List<Job> t = new List<Job>() { new Job(1, 1, 3, 1), new Job(0, 1, 1, 1), new Job(2, 1, 5, 2), new Job(3, 1, 5, 0) };
-            //JobByBurstComparer c = new JobByBurstComparer();
-            //t.Sort(c);
         }
 
         private void btnAddProcess_Click(object sender, RoutedEventArgs e)
@@ -51,31 +49,52 @@ namespace SchedulingAlgorithms
             dgProcessesTable.Items.Refresh();
         }
 
+        private void CheckJobQueueSize()
+        {
+            if (jobQueue.GetSize() > 1) btnStartSimulation.IsEnabled = true;
+        }
+
         private void rbFCFS_Checked(object sender, RoutedEventArgs e)
         {
             simulation.Algorithm = new FCFS(jobQueue);
+            CheckJobQueueSize();
         }
 
         private void rbRoundRobin_Checked(object sender, RoutedEventArgs e)
         {
             simulation.Algorithm = new RoundRobin(jobQueue);
+            CheckJobQueueSize();
         }
 
         private void rbSJF_Checked(object sender, RoutedEventArgs e)
         {
             simulation.Algorithm = new SJF(jobQueue);
+            CheckJobQueueSize();
         }
 
         private void rbPriority_Checked(object sender, RoutedEventArgs e)
         {
             simulation.Algorithm = new Priority(jobQueue);
+            CheckJobQueueSize();
         }
 
         private void btnStartSimulation_Click(object sender, RoutedEventArgs e)
         {
-            chart.Draw(simulation);
-            simulation.Reset();
+            chart.Draw();
             dgProcessesTable.Items.Refresh();
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            simulation.Reset();
+            jobQueue.Clear();
+            chart.Clear();
+            dgProcessesTable.Items.Refresh();
+            rbFCFS.IsChecked = false;
+            rbPriority.IsChecked = false;
+            rbRoundRobin.IsChecked = false;
+            rbSJF.IsChecked = false;
+            btnStartSimulation.IsEnabled = false;
         }
     }
 }
